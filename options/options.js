e889d9392ee68500fc58a590486bf5dbcf517b66
Aamir  Padder <aamir.padder.dms@crowdbotics.com>
2023-04-21 16:00:24 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "testappcb-40041",
   "url": "https://testappcb-40041.botics.co",
   "api": "https://testappcb-40041.botics.co/api/v1",
-  "initialRoute": "productListing"
+  "initialRoute": "product-listing"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
